549a0278607e569baef41dc9dcd58435f731aad1
VvanHarn <143938565+VvanHarn@users.noreply.github.com>
2023-09-22 13:15:51 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,19 @@
 // JavaScript Document
 console.log("hi");
+
+/******************************/
+/* menu openen de MENU button */
+/******************************/
+
+/* JOUW CODE HIER - stap 4 */
+
+// stap 1: zoek de menu-button op en sla die op in een variabele
+var menuButton = document.querySelector("header nav button");
+var hetMenu = document.querySelector("nav");
+// stap 2: laat de menu-button luisteren naar kliks en voer dan een functie uit
+menuButton.onclick = toggleMenu;
+
+// stap 3: voeg in de functie een class toe aan de nav
+function toggleMenu () {
+  hetMenu.classList.toggle("open");
+}
